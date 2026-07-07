15ebb4e3f1c4bd727884d23155dfd93b5a215cd8
KLBSTL <m13477484215@163.com>
2026-07-07 16:55:44 +0800

    完善tools内调用工具的函数和类

--- fourth/utils/tools.py
+++ fourth/utils/tools.py
@@ -1,7 +1,9 @@
 import os
 from typing import Dict, Any
-
+from dotenv import load_dotenv
 from serpapi import SerpApiClient
+
+load_dotenv()
 
 def search(query: str) -> str:
     """
@@ -57,9 +59,40 @@ class ToolExecutor:
         self.tools[name] = {"description": description, "func": func}
         print(f"工具 '{name}' 已注册。")
 
+    def getTool(self, name: str) -> callable:
+        return self.tools.get(name,{}).get("func")
+
+    def getAvailableTools(self) -> str:
+        return "\n".join([
+            f"- {name}: {info['description']}"
+            for name, info in self.tools.items()
+        ])
 
 
+if __name__ == '__main__':
+    # 1. 初始化工具执行器
+    toolExecutor = ToolExecutor()
 
+    # 2. 注册我们的实战搜索工具
+    search_description = "一个网页搜索引擎。当你需要回答关于时事、事实以及在你的知识库中找不到的信息时，应使用此工具。"
+    toolExecutor.registerTool("Search", search_description, search)
+
+    # 3. 打印可用的工具
+    print("\n--- 可用的工具 ---")
+    print(toolExecutor.getAvailableTools())
+
+    # 4. 智能体的Action调用，这次我们问一个实时性的问题
+    print("\n--- 执行 Action: Search['英伟达最新的GPU型号是什么'] ---")
+    tool_name = "Search"
+    tool_input = "英伟达最新的GPU型号是什么"
+
+    tool_function = toolExecutor.getTool(tool_name)
+    if tool_function:
+        observation = tool_function(tool_input)
+        print("--- 观察 (Observation) ---")
+        print(observation)
+    else:
+        print(f"错误:未找到名为 '{tool_name}' 的工具。")
 
 
 
